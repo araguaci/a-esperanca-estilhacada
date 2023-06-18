dee395958c935bfcbc83a8b68291f3a08c3a52e8
araguaci <araguaci@gmail.com>
2023-06-18 18:24:04 -0300

    A Esperança Estilhaçada por Augusto Nunes

---
+++ src/components/PdfViewer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.openPdf()
+  },
+  methods: {
+    openPdf() {
+      const file = this.$route.params.file
+      const pdfUrl = '/a_esperanca_estilhacada' + file + '.pdf'
+      // window.open(pdfUrl, '_blank')
+    }
+  }
+}
+</script>
